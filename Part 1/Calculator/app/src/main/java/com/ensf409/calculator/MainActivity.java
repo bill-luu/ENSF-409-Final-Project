b3d04f99aa40ca128b9cc0568625f183f156d3d2
Bill Luu <billluu619@gmail.com>
2017-03-26 19:26:36 -0600

    Added Java Code for Ui components

--- Part 1/Calculator/app/src/main/java/com/ensf409/calculator/MainActivity.java
+++ Part 1/Calculator/app/src/main/java/com/ensf409/calculator/MainActivity.java
@@ -8,8 +8,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	    private TextView valueTextView;
+	    private Button oneButton;
+        private Button twoButton;
+        private Button threeButton;
+		private Button fourButton;
+		private Button fiveButton;
+		private Button sixButton;
+		private Button sevenButton;
+		private Button eightButton;
+		private Button nineButton;
+		private Button zeroButton;
+		private Button clearButton;
+		private Button addButton;
+		private Button divideButton;
+		private Button multiplyButton;
+		private Button minusButton;
+		private Button equalsButton;
+        private Button decimalButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +38,150 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupView();
+    }
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    public void setupView()
+    {
+    	valueTextView = (TextView)findViewById(R.id.ValueTextView);
+
+        //Initialize Buttons
+        oneButton = (Button)findViewById(R.id.OneButton);
+        twoButton = (Button)findViewById(R.id.TwoButton);
+        threeButton = (Button)findViewById(R.id.ThreeButton);
+		fourButton = (Button)findViewById(R.id.FourButton);
+		fiveButton = (Button)findViewById(R.id.FiveButton);
+		sixButton = (Button)findViewById(R.id.SixButton);
+		sevenButton = (Button)findViewById(R.id.SevenButton);
+		eightButton = (Button)findViewById(R.id.EightButton);
+		nineButton = (Button)findViewById(R.id.NineButton);
+		zeroButton = (Button)findViewById(R.id.ZeroButton);
+		clearButton = (Button)findViewById(R.id.ClearButton);
+		addButton = (Button)findViewById(R.id.AddButton);
+		divideButton = (Button)findViewById(R.id.DivideButton);
+		multiplyButton = (Button)findViewById(R.id.MultiplyButton);
+		minusButton = (Button)findViewById(R.id.MinusButton);
+		equalsButton = (Button)findViewById(R.id.EqualsButton);
+        decimalButton = (Button)findViewById((R.id.DecimalButton));
+
+        //Add listeners
+        //TODO - Complete definition of ActionListeners
+        oneButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public  void onClick(View view){
+                
+            }
+        });
+
+        twoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        threeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        fourButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        fiveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        sixButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        sevenButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        eightButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        nineButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        zeroButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        decimalButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                
+            }
+        });
+
+        equalsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+
             }
         });
     }
